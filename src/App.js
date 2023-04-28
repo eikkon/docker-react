810e6216026d72bbc96c48e591f14b1955a45b19
eikkon <eikkon@DESKTOP-ECIJ9TR.localdomain>
2023-04-28 07:37:12 +0200

    APP JS UPDATE

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi toto bienvenue dans node.js
+          Hi JOUJOU bienvenue dans node.js
         </p>
         <a
           className="App-link"
